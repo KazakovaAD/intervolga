ad200fc2736e29f97f4a1d6c8f49cec265b7f96d
KazakovaAD <kazakova.nd@mail.ru>
2020-11-26 01:05:32 +0400

    3-4

---
+++ four/array.php
@@ -0,0 +1,19 @@
+<?php
+$array = [];
+$permitted_chars = '0123456789abcdefghijklmnopqrstuvwxyz';
+for($i = 0; $i<100; $i++)
+{
+    $array[$i] =  substr(str_shuffle($permitted_chars), 0, 1);
+}
+$count = 0;
+for ($i = 0; $i<99; $i++)
+{
+    if ($array[$i] === $array[$i+1])
+    {
+        $count++;
+    }
+
+}
+foreach ($array as $value)
+    echo $value;
+echo "<br/>Количество пар: ", $count;
--- two/image.php
+++ two/image.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" type="text/css" href="\intervolga\two\css\img.css">
 <div class="image">
     <img src="/intervolga/two/image/image.png">
 </div>
